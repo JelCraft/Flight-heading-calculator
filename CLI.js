d4342525622d507e1c1040ede1e919f4d7085abb
JelCraft <Jelcraft@outlook.com>
2022-02-03 22:37:21 +0100

    Updated flight heading and glide ratio on CLI

--- CLI.js
+++ CLI.js
@@ -18,47 +18,34 @@ const options = yargs
 const ReplyInput = `Starting position: ${options.ax}, ${options.az}. Destination position: ${options.bx}, ${options.bz}`;
 
 function computeHeading() {
+    const coords = getCoords();
     let angleRad = Math.atan((coords.ax-coords.bx)/(coords.az-coords.bz));
-    let angleDeg1 = angleRad * 180 / Math.PI;
-    let angleDeg2 = angleDeg + 180
-    let angleDegCorrected
+    let angleDeg
 
     if(0 < coords.bx-coords.ax && 0 < coords.bz-coords.az) { //Quadrant NE
-        angleDegCorrected = angleRad * 180 / Math.PI
-    } else { //Quadrant SW
-        angleDegCorrected = angleRad * 180 / Math.PI + 180
+        angleDeg = -1 * angleRad * 180 / Math.PI + 180;
+        console.log("Quadrant NE");
+    } else if(0 > coords.bx-coords.ax && 0 > coords.bz-coords.az){ //Quadrant SW
+        angleDeg = -1 * angleRad * 180 / Math.PI - 270;
+        console.log("Quadrant SW");
     }
     if(coords.bx-coords.ax > 0 && coords.bz-coords.az < 0) { //Quadrant SE
-        angleDegCorrected = angleRad * 180 / Math.PI + 90
-    } else { //Quadrant NW
-        angleDegCorrected = angleRad * 180 / Math.PI + 270
+        angleDeg = angleRad * 180 / Math.PI;
+        console.log(`Quadrant SE, ${coords.bx} - ${coords.ax}, ${coords.bz} - ${coords.az}`);
+    } else if(coords.bx-coords.ax < 0 && coords.bz-coords.az > 0) { //Quadrant NW
+        angleDeg = angleRad * 180 / Math.PI - 180;
+        console.log(`Quadrant NW, ${coords.bx} - ${coords.ax}, ${coords.bz} -${coords.az}`);
     }
 
-
-
-    //Fix negative or >360 angles
     if(angleDeg<0) {
         angleDeg = 360-angleDeg;
     }
     if(angleDeg>=360) {
-        angleDeg -=360;
-    }
-
-    if(angleDeg2<0) {
-        angleDeg2 = 360-angleDeg;
-    }
-    if(angleDeg2>=360) {
-        angleDeg2 -=360;
-    }
-    if(angleDegCorrected<0) {
-        angleDegCorrected = 360-angleDegCorrected;
-    }
-    if(angleDegCorrected>=360) {
-        angleDegCorrected -= 360
+        angleDeg -= 360
     }
 
     console.log(ReplyInput);
-    console.log(`Flight heading ${angleDeg1} / ${angleDeg2} (Corrected heading: ${angleDegCorrected})`);
+    console.log(`Flight heading ${angleDeg}`);
 }
 
 let distance
@@ -69,7 +56,7 @@ function computeDistance() {
 }
 
 function computeAltitude() {
-    console.log(`Glide ratio 7.5:1, Starting altitude (AGL) → ${distance/7.5}`)
+    console.log(`Glide ratio 8.5:1, Starting altitude (AGL) → ${distance/8.5}`)
 }
 
 function computeFlightTime() {
